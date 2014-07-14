e2e9f4b29b2ab9f2f906a95781ed69619fb0e298
Janic Beauchemin <janic_33@msn.com>
2014-07-14 01:13:22 -0400

    add options to set / get option params minor css changes add scss vars

--- dist/switcher.js
+++ dist/switcher.js
@@ -58,34 +58,85 @@
         },
 
         buildHtml: function (el, settings) {
-            var checked = "";
-
             $(el).addClass(settings.class);
 
-            if (settings.selected) {
-                $(el).addClass('is-active');
-                checked = 'checked';
-            }
-
-            if (settings.disabled) {
-                $(el).addClass('is-disabled');
-            }
-
             $(el).html(
-                '<input type="checkbox" name="' + settings.name + '" value="' + settings.name + '" '+ checked + '>' +
+                '<input type="checkbox" name="' + settings.name + '" value="' + settings.name + '">' +
                 '<div class="content clearfix">' +
                     '<div class="slider"></div>' +
                     '<span class="text textYes">' + copy[settings.language].yes + '</span>' +
                     '<span class="text textNo">' + copy[settings.language].no + '</span>' +
                 '</div>'
             );
+
+            if (settings.selected) {
+                this.setValue(true);
+            }
+
+            if (settings.disabled) {
+                this.setDisabled(true);
+            }
+        },
+
+        setValue: function (val) {
+            if (typeof val != 'boolean') {
+                return console.log('The parameter need to be true or false as a boolean');
+            }
+
+            $(this.element).find('input').prop("checked", val);
+            this.settings.selected = val;
+
+            if (val === true) {
+                $(this.element).addClass('is-active');
+            } else {
+                $(this.element).removeClass('is-active');
+            }
+        },
+
+        getValue: function () {
+            return this.settings.selected;
+        },
+
+        setDisabled: function (val) {
+            if (typeof val != 'boolean') {
+                return console.log('The parameter need to be true or false as a boolean');
+            }
+
+            this.settings.disabled = val;
+
+            if (val === true) {
+                $(this.element).addClass('is-disabled');
+            } else {
+                $(this.element).removeClass('is-disabled');
+            }
+        },
+
+        getDisabled: function () {
+            return this.settings.disabled;
+        },
+
+        setLanguage: function () {
+
+        },
+
+        getLanguage: function () {
+
         }
     });
 
     $.fn[ pluginName ] = function (options) {
+        // http://stackoverflow.com/questions/12880256/jquery-plugin-creation-and-public-facing-methods
+        var args = Array.prototype.slice.call(arguments, 1);
         this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            var item = $(this);
+            var instance = item.data("plugin_" + pluginName);
+
+            if (!instance) {
+                item.data("plugin_" + pluginName, new Plugin(this, options));
+            } else {
+                if(typeof options === 'string') {
+                    instance[options].apply(instance, args);
+                }
             }
         });
 
